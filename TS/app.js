2db1e86458c3c80b2f9c4f7dd3838a3561ab79ad
Aryan <aryan2563737@gmail.com>
2020-11-09 23:12:08 +0530

    team density = 2

--- TS/app.js
+++ TS/app.js
@@ -258,34 +258,31 @@ const card_credentials = [
     ['Ron Weasley', 'IMG/Team/weasley.jpg', 'Auror', 'The Boy who Lived. The Boy who cheated death twice. The master of Deathly Hallows.'],
     ['Hermoine Granger', 'IMG/Team/hermione.jpg', 'Minister Of Magic', 'The brightest witch of her age. The founder of S.P.E.W'],
     ['Harry Potter', 'IMG/Team/potter.jpg', 'Auror', 'The Boy who Lived. The Boy who cheated death twice. The master of Deathly Hallows.'],
-    ['Ron Weasley', 'IMG/Team/weasley.jpg', 'Auror', 'The Boy who Lived. The Boy who cheated death twice. The master of Deathly Hallows.']
+    ['Ron Weasley', 'IMG/Team/weasley.jpg', 'Auror', 'The Boy who Lived. The Boy who cheated death twice. The master of Deathly Hallows.'],
+    ['Hermoine Granger', 'IMG/Team/hermione.jpg', 'Minister Of Magic', 'The brightest witch of her age. The founder of S.P.E.W']
 ];
 const render_card = (i = 0) => {
     //?Changes the  name of all three cards
     name1.innerHTML = card_credentials[i][0];
     name2.innerHTML = card_credentials[i + 1][0];
-    name3.innerHTML = card_credentials[i + 2][0];
     //?Changes the image of three cards 
     img1.src = card_credentials[i][1];
     img2.src = card_credentials[i + 1][1];
-    img3.src = card_credentials[i + 2][1];
     //?changes the designation of three cards
     designation1.innerHTML = card_credentials[i][2];
     designation2.innerHTML = card_credentials[i + 1][2];
-    designation3.innerHTML = card_credentials[i + 2][2];
     //?Changes the about section of cards
     about1.innerHTML = card_credentials[i][3];
     about2.innerHTML = card_credentials[i + 1][3];
-    about3.innerHTML = card_credentials[i + 2][3];
 };
 radio1.addEventListener('click', () => {
     render_card(0);
 });
 radio2.addEventListener('click', () => {
-    render_card(1);
+    render_card(2);
 });
 radio3.addEventListener('click', () => {
-    render_card(2);
+    render_card(4);
 });
 //?Nav Management
 const nav_close = document.getElementById('close');
